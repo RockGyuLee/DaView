39a15c54834a58972c9fa8cd18ce99f2fb5b9042
RockGyuLee <rmfoszld@naver.com>
2024-01-09 21:25:01 +0900

    첫 Commit

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import {
+    View,
+    Text
+} from "react-native";
+
+export function DateWithPeople(){
+
+    return(
+        <View>
+            <Text className='text-blue-500'>
+            Test
+            </Text>
+        </View>
+    )
+}
